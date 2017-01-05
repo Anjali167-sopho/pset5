1cb3f86880c21ae4a8232ee49b92d79ffcbd5b74
Anjali167-sopho <anjalijha167@gmail.com>
2017-01-05 13:24:25 +0000

    load function modified

--- dictionary.c
+++ dictionary.c
@@ -23,10 +23,52 @@ bool check(const char* word)
 /**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
-bool load(const char* dictionary)
+ bool load(const char* dictionary)
 {
     // TODO
-    return false;
+    // opens dictionary
+    FILE* file = fopen(dictionary, "r");
+    if (file == NULL)
+        return false;
+    
+    // create an array for word to be stored in
+    char word[LENGTH+1];
+    
+    // scan through the file, loading each word into the hash table
+    while (fscanf(file, "%s\n", word)!= EOF)
+    {
+        // increment dictionary size
+        dictionarySize++;
+        
+        // allocate memory for new word 
+        node* Word = malloc(sizeof(node));
+        
+        // put word in the new node
+        strcpy(Word->word, word);
+        
+        // find what index of the array the word should go in
+        int count = hash(word);
+        
+        // if hashtable is empty at index, insert
+        if (hashtable[count] == NULL)
+        {
+            hashtable[count] = Word;
+            Word->next = NULL;
+        }
+        
+        // if hashtable is not empty at index, append
+        else
+        {
+            Word->next = hashtable[index];
+            hashtable[index] = Word;
+        }      
+    }
+    
+    // close file
+    fclose(file);
+    
+    // return true if successful 
+    return true;
 }
 
 /**
